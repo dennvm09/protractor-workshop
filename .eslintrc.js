d41299f34b26ee684f15a598fc6ee281c29c2728
dennvm09 <valenmosquera1999@gmail.com>
2022-01-24 16:10:04 -0500

    .eslintrc file type was change to .ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,20 @@
-// eslint-disable-next-line no-undef
-module.exports ={
-  "env": {
-    "browser": true,
-    "es2021": true
-  },
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:protractor/recommended"
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": "latest"
-  },
-  "plugins": [
-    "@typescript-eslint",
-    "protractor"
-  ],
-  "rules": {
-    "comma-dangle": "off",
-    "@typescript-eslint/comma-dangle": ["error", "only-multiline"]
-  }
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
 }
