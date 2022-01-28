28d553be8f4fc785de6118920e616991a9586958
dennvm09 <valenmosquera1999@gmail.com>
2022-01-28 12:20:42 -0500

    unnecessary functions (browser.wait()) were deleted

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -21,30 +21,30 @@ describe('Buy a t-shirt', () => {
     const passEle = browser.findElement(by.id('password'));
     const loginBtnEle = browser.findElement(by.id('login-button'));
 
-    await browser.wait(userEle.sendKeys(userName), 4000, "Invalid user.");
-    await browser.wait(passEle.sendKeys(passwd), 4000, "Invalid password.");
-    await browser.wait(loginBtnEle.click(), 3000, "Not possible to access.");
+
+    await userEle.sendKeys(userName);
+    await passEle.sendKeys(passwd);
+    await loginBtnEle.click();
 
     const buyBtnEle = browser.findElement(by.id('add-to-cart-sauce-labs-bolt-t-shirt'));
-    await browser.wait(buyBtnEle.click(), 4000, "Not possible to access.");
+    await buyBtnEle.click();
     const cartBtnEle = browser.findElement(by.id('shopping_cart_container'));
-    await browser.wait(cartBtnEle.click(), 4000, "Not possible to access.");
+    await cartBtnEle.click();
     const checkBtnEle = browser.findElement(by.id('checkout'));
-    await browser.wait(checkBtnEle.click(), 4000, "Not possible to access.");
+    await checkBtnEle.click();
 
     const firtNameEle = browser.findElement(by.id('first-name'));
     const lastNameEle = browser.findElement(by.id('last-name'));
     const zipCodeEle = browser.findElement(by.id('postal-code'));
     const continueBtnEle = browser.findElement(by.id('continue'));
 
-    await browser.wait(firtNameEle.sendKeys(firstName), 4000, "Invalid name.");
-    await browser.wait(lastNameEle.sendKeys(lastName), 4000, "Invalid lastname.");
-    await browser.wait(zipCodeEle.sendKeys(zipCode), 4000, "Invalid zipcode.");
-    await browser.wait(continueBtnEle.click(), 3000, "Not possible to access.");
+    await firtNameEle.sendKeys(firstName);
+    await lastNameEle.sendKeys(lastName);
+    await zipCodeEle.sendKeys(zipCode);
+    await continueBtnEle.click();
 
     const finishBtnEle = browser.findElement(by.id('finish'));
-    await browser.wait(finishBtnEle.click(), 3000, "Not possible to access.");
-
+    await finishBtnEle.click();
   });
   it('then should be bought a t-shirt', async () => {
     expect(await browser.findElement(by.className('complete-text')).getText()).toBe('Your order has been dispatched, and will arrive just as fast as the pony can get there!');
